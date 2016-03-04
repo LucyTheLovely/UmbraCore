3fcffdb7b865b99569b7f747f3bd29175a6c391c
BuildTools <1koenenjacob@gmail.com>
2016-03-04 08:45:58 -0500

    Finished launchpads, stable commit. Bumped version.

--- main/java/com/umbrafactions/umbracore/shortcuts/PlayerScoreboard.java
+++ main/java/com/umbrafactions/umbracore/shortcuts/PlayerScoreboard.java
@@ -10,24 +10,20 @@ public class PlayerScoreboard {
     private ScoreboardManager manager = Bukkit.getScoreboardManager();
     private Scoreboard board = manager.getNewScoreboard();
     private Objective playerInfoObjective = board.registerNewObjective("playerinfo", "dummy");
-    private Objective prefixObjective = board.registerNewObjective("playerprefix", "dummy");
 
     private Score prefix = null;
-    private Score nameTagPrefix = null;
     private Score money = null;
     private Score dividerOne = null;
 
     public void setPlayerInfoScoreboard(Player p) {
         String prefixText = Core.colorize(Core.chat.getPlayerPrefix(p));
         prefixText = prefixText.replace("[", "").replace("]", "");
-        prefixObjective.setDisplaySlot(DisplaySlot.BELOW_NAME);
         playerInfoObjective.setDisplaySlot(DisplaySlot.SIDEBAR);
         playerInfoObjective.setDisplayName(ChatColor.GRAY + "Info for " + ChatColor.YELLOW + p.getName());
         money = playerInfoObjective.getScore(
                 ChatColor.LIGHT_PURPLE + "Balance: " + ChatColor.GREEN + Core.econ.format(Core.econ.getBalance(p))
         );
         prefix = playerInfoObjective.getScore(ChatColor.LIGHT_PURPLE + "Rank: " + prefixText);
-        nameTagPrefix = prefixObjective.getScore(prefixText);
         dividerOne = playerInfoObjective.getScore("");
         money.setScore(1);
         prefix.setScore(2);
@@ -38,17 +34,14 @@ public class PlayerScoreboard {
     public void updatePlayerInfoScoreboard(Player p) {
         Scoreboard board = manager.getNewScoreboard();
         Objective playerInfoObjective = board.registerNewObjective("playerinfo", "dummy");
-        Objective prefixObjective = board.registerNewObjective("playerprefix", "dummy");
         String prefixText = Core.colorize(Core.chat.getPlayerPrefix(p));
         prefixText = prefixText.replace("[", "").replace("]", "");
-        prefixObjective.setDisplaySlot(DisplaySlot.BELOW_NAME);
         playerInfoObjective.setDisplaySlot(DisplaySlot.SIDEBAR);
         playerInfoObjective.setDisplayName(ChatColor.GRAY + "Info for " + ChatColor.YELLOW + p.getName());
         money = playerInfoObjective.getScore(
                 ChatColor.LIGHT_PURPLE + "Balance: " + ChatColor.GREEN + Core.econ.format(Core.econ.getBalance(p))
         );
         prefix = playerInfoObjective.getScore(ChatColor.LIGHT_PURPLE + "Rank: " + prefixText);
-        nameTagPrefix = prefixObjective.getScore(prefixText);
         dividerOne = playerInfoObjective.getScore("");
         money.setScore(1);
         prefix.setScore(2);
